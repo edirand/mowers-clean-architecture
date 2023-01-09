0168f21d5fcb84180b5dde8014412b556b3d323b
Emilien Dirand <emilien.dirand21@gmail.com>
2023-01-09 17:25:33 +0100

    feat: add a console application to run the program

---
+++ src/Core/Mowers.CleanArchitecture.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mowers.CleanArchitecture.Application;
+
+/// <summary>
+/// Extension class to inject necessary services in DI container.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class ApplicationServiceRegistration
+{
+    /// <summary>
+    /// Injects necessary services in DI container.
+    /// </summary>
+    /// <param name="services">The services collection.</param>
+    /// <returns>The configured services collection.</returns>
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services
+            .AddMediatR(AppDomain.CurrentDomain.GetAssemblies())
+            ;
+    }
+}
---
+++ src/UI/Mowers.CleanArchitecture.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Mowers.CleanArchitecture.Application;
+using Mowers.CleanArchitecture.Application.Features.Mowers.Commands.ProcessFile;
+
+var serviceProvider = new ServiceCollection()
+        .AddApplicationServices()
+        .BuildServiceProvider()
+    ;
+
+var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+var filePath = args[0];
+var file = File.OpenRead(filePath);
+
+var result = await mediator.Send(new ProcessFileCommand(file));
+
+foreach (var mower in result.Mowers)
+{
+    Console.WriteLine(mower);
+}
+
+Console.ReadLine();
