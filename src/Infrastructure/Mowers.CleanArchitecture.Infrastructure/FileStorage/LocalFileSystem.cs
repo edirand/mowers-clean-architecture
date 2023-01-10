7af918ee27cc7e398a6e41b1bc4f3df9b12f0a5d
edirand <emilien.dirand21@gmail.com>
2023-01-10 01:07:33 +0100

    feat: add local file storage
    Add an infrastructure layer to add local file storage.

---
+++ src/Infrastructure/Mowers.CleanArchitecture.Infrastructure/FileStorage/LocalFileSystem.cs
@@ -0,0 +1,33 @@
+﻿using Mowers.CleanArchitecture.Application.Contracts.Infrastructure.FileStorage;
+
+namespace Mowers.CleanArchitecture.Infrastructure.FileStorage;
+
+/// <summary>
+/// An implementation of <see cref="IFileStorage"/> using local file system.
+/// </summary>
+public class LocalFileSystem : IFileStorage
+{
+    private const string Directory = "storage";
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="LocalFileSystem"/> class.
+    /// </summary>
+    public LocalFileSystem()
+    {
+        var info = new DirectoryInfo(Directory);  
+        if (!info.Exists) {  
+            info.Create();  
+        }
+    }
+
+    /// <inheritdoc />
+    public Task<string> Store(Stream data)
+    {
+        var fileName = Path.GetRandomFileName();
+        var filePath = $"{Directory}/{fileName}";
+        using var outputFileStream = new FileStream(filePath, FileMode.Create);
+        data.CopyTo(outputFileStream);
+
+        return Task.FromResult(fileName);
+    }
+}
---
+++ src/Infrastructure/Mowers.CleanArchitecture.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Mowers.CleanArchitecture.Application.Contracts.Infrastructure.FileStorage;
+using Mowers.CleanArchitecture.Infrastructure.FileStorage;
+
+namespace Mowers.CleanArchitecture.Infrastructure;
+
+/// <summary>
+/// Extension class to inject necessary services in DI container.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class InfrastructureServiceRegistration
+{
+    
+    /// <summary>
+    /// Injects necessary services in DI container.
+    /// </summary>
+    /// <param name="services">The services collection.</param>
+    /// <returns>The configured services collection.</returns>
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services
+                .AddSingleton<IFileStorage, LocalFileSystem>()
+            ;
+    }
+}
