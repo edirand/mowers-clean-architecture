1248761c8c315866314412143c4bab024fcae743
edirand <emilien.dirand21@gmail.com>
2023-01-10 02:32:41 +0100

    feat: add API versioning

--- src/API/Mowers.CleanArchitecture.Api/StartupExtensions.cs
+++ src/API/Mowers.CleanArchitecture.Api/StartupExtensions.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Mowers.CleanArchitecture.Api.Swagger;
 using Mowers.CleanArchitecture.Application;
 using Mowers.CleanArchitecture.Infrastructure;
 using Mowers.CleanArchitecture.Persistence;
 
 namespace Mowers.CleanArchitecture.Api;
 
+/// <summary>
+/// Extensions to configure startup.
+/// </summary>
 public static class StartupExtensions
 {
+    /// <summary>
+    /// Configures services.
+    /// </summary>
     public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.Services
@@ -24,6 +33,7 @@ public static class StartupExtensions
                     .AllowAnyMethod()
                 );
             })
+            .AddVersioning()
             .AddSwagger()
             .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies())
             ;
@@ -32,27 +42,59 @@ public static class StartupExtensions
         return builder;
     }
 
+    private static IServiceCollection AddVersioning(this IServiceCollection services)
+    {
+        return services
+                .AddApiVersioning(opt =>
+                {
+                    opt.DefaultApiVersion = new ApiVersion(1, 0);
+                    opt.AssumeDefaultVersionWhenUnspecified = true;
+                    opt.ReportApiVersions = true;
+                    opt.ApiVersionReader = ApiVersionReader.Combine(
+                            new UrlSegmentApiVersionReader(),
+                            new HeaderApiVersionReader("x-api-version")
+                        )
+                        ;
+                })
+                .AddVersionedApiExplorer(setup =>
+                {
+                    setup.GroupNameFormat = "'v'VVV";
+                    setup.SubstituteApiVersionInUrl = true;
+                })
+                .AddEndpointsApiExplorer()
+            ;
+    }
+
     private static IServiceCollection AddSwagger(this IServiceCollection services)
     {
         return services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo
             {
-                Version = "v1",
-                Title = "Mowers Clean Architecture API",
-            });
-            c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, typeof(StartupExtensions).Assembly.GetName().Name + ".xml"));
-        });
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+                    typeof(StartupExtensions).Assembly.GetName().Name + ".xml"));
+            })
+            .ConfigureOptions<ConfigureSwaggerOptions>();
     }
 
+    /// <summary>
+    /// Configures the application.
+    /// </summary>
     public static WebApplication ConfigureApplication(this WebApplication app)
     {
+        var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
         app
             .UseHttpsRedirection()
             .UseRouting()
             .UseCors("Open")
             .UseSwagger()
-            .UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mowers Clean Architecture API"); })
+            .UseSwaggerUI(c =>
+            {
+                foreach (var groupName in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse()
+                             .Select(x => x.GroupName))
+                {
+                    c.SwaggerEndpoint($"/swagger/{groupName}/swagger.json",
+                        groupName.ToUpperInvariant());
+                }
+            })
             ;
 
         app.MapControllers();
---
+++ src/API/Mowers.CleanArchitecture.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Mowers.CleanArchitecture.Api.Swagger;
+
+/// <summary>
+/// Options to configure swagger document generation.
+/// </summary>
+public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="ConfigureSwaggerOptions"/>.
+    /// </summary>
+    public ConfigureSwaggerOptions(
+        IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+    
+    /// <summary>
+    /// Configure each API discovered for Swagger Documentation.
+    /// </summary>
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add swagger document for every API version discovered
+        foreach (var description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                CreateVersionInfo(description));
+        }
+    }
+
+    /// <summary>
+    /// Configure Swagger Options.
+    /// </summary>
+    public void Configure(string? name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    /// <summary>
+    /// Create information about the version of the API.
+    /// </summary>
+    private static OpenApiInfo CreateVersionInfo(
+        ApiVersionDescription desc)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = "Mowers Clean Architecture API",
+            Version = desc.ApiVersion.ToString()
+        };
+
+        if (desc.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated. Please use one of the new APIs available from the explorer.";
+        }
+
+        return info;
+    }
+}
